99f08059f2c432f14aa21ecd21645888d7c9809f
race <racechao@gmail.com>
2016-03-06 23:58:58 +0800

    Add sort

--- Functions/Functions.playground/Contents.swift
+++ Functions/Functions.playground/Contents.swift
@@ -31,3 +31,30 @@ func isEven<T: IntegerType>(i: T) -> Bool {
 let int8isEven: Int8 -> Bool = isEven
 
 // Tips: 闭包是一种特殊的方法（匿名方法），闭包与方法主要的不同点是`闭包`能够捕获变量
+
+
+// 一个粒子
+// 利用 Objective-C 的 runtime
+let last = "lastName", first = "firstName"
+
+let people = [
+    [first: "Neil", last: "Chao"],
+    [first: "Yu",last: "Yu"],
+    [first: "Chao", last: "Race"]
+]
+
+let lastDescriptor = NSSortDescriptor(key: last, ascending: true, selector: "localizedCaseInsensitiveCompare:")
+let firstDescriptor = NSSortDescriptor(key: first, ascending: true, selector: "localizedCaseInsensitiveCompare:")
+
+let descriptor = [lastDescriptor, firstDescriptor]
+let sortedArray = (people as NSArray).sortedArrayUsingDescriptors(descriptor)
+
+var strings = ["Hello", "hallo", "Hallo","hello"]
+
+strings.sortInPlace {
+    return $0.localizedCaseInsensitiveCompare($1) == .OrderedAscending
+}
+
+let sortedArrays = people.sort { $0[last] < $1[last] }
+
+sortedArrays
